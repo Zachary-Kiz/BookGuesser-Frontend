2a34471be3812c4294cb7635df9ae156cddbd3c5
Zachary-Kiz <zachary.kizell@gmail.com>
2026-04-27 20:06:23 -0400

    add basic game logic

--- app/today/page.tsx
+++ app/today/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import {getTodayPuzzle} from "@/api/todayPuzzle";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import styles from "./Puzzle.module.css"
 
 export default function Today() {
@@ -10,10 +10,42 @@ export default function Today() {
     const [error, setError] = useState<any>();
     const [guesses, setGuesses] = useState<Array<string>>([]);
     const [currentGuess, setCurrentGuess] = useState<string>("");
+    const [searchRes, setSearchRes] = useState<Array<string>>([])
+    const [isGuessed, setIsGuessed] = useState<boolean>(false);
 
-    function guessBook() : void {
-        setGuesses([...guesses, currentGuess])
-        setCurrentGuess("")
+    const bookTitle : string = book?.title;
+
+    const level : number = isGuessed ? 6 : guesses.length;
+
+    const debounce = <T extends unknown[]>(
+        callback: (...args: T) => void,
+        delay: number,
+        ) => {
+            let timeoutTimer: ReturnType<typeof setTimeout>;
+            
+            return (...args: T) => {
+                clearTimeout(timeoutTimer);
+            
+                timeoutTimer = setTimeout(() => {
+                callback(...args);
+                }, delay);
+            };
+        };
+
+    async function getBookNames(query: string) {
+        const res = await fetch(`https://openlibrary.org/search.json?q=${query}&limit=10`, {
+        })
+        const resJson = await res.json();
+        const resItems : string[] = resJson['docs']?.map((item: { title: any; }) => item?.title) || [];
+        const removeDup : string[] = [...new Set(resItems)];
+        setSearchRes(removeDup);
+    }
+
+    function checkGuess(guess : string) : void {
+        if (bookTitle && guess === bookTitle) {
+            setIsGuessed(true);
+        }
+        setGuesses([...guesses, guess]);
     }
 
     useEffect(() => {
@@ -27,18 +59,27 @@ export default function Today() {
         
     }, []);
 
-    console.log(currentGuess)
+    const debouncedSearch = useMemo(
+        () =>
+            debounce((val: string) => {
+                getBookNames(val);
+            }, 150),
+        []
+    );
 
     return (
         <div>
+            {isGuessed && <div>You Win!!</div>}
             {book && 
-                <div>
-                    <div><img src={book.covers[0].imageUrl}/></div>
+                <div><img src={book.covers[level].imageUrl}/></div>
                     
-                </div>
             }
-            <input type="text" value={currentGuess} onChange={(e) => setCurrentGuess(e.target.value)}></input>
-            <button onClick={() => guessBook()}>Enter</button>
+            <input list="books" type="text" value={currentGuess} onChange={(e) => {const val = e.target.value; setCurrentGuess(val); debouncedSearch(val)}}></input>
+            <div id="books">
+                {searchRes.map((title: string) => {
+                    return <div onClick={() => checkGuess(title)} key={title}>{title}</div>
+                })}
+            </div>
             <div className={styles.puzzleButtons}>
                 {guesses?.map(guess => {
                     return <div key={guess}>{guess}</div>
