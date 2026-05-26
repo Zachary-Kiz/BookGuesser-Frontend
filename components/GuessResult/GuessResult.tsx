cfcff93c15c90609cd645d236910ca309345beb4
Zachary-Kiz <zachary.kizell@gmail.com>
2026-05-26 16:09:02 -0400

    display Skipped! if user guesses with no text

--- components/GuessResult/GuessResult.tsx
+++ components/GuessResult/GuessResult.tsx
@@ -8,7 +8,7 @@ export default function GuessResult({guess}: Guess) {
     return (
         <div className="flex flex-row items-center gap-2 border-(--dark-brown) border-[0.5] rounded-sm pl-2">
             <div className={styles.ex}>&#215;</div>
-            <div className="" key={guess}>{guess}</div>
+            <div className="" key={guess}>{guess ? guess : "Skipped!"}</div>
         </div> 
     )
 }
