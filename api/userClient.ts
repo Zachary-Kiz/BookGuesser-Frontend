1925fe7b4eacb1b8be237c61153f5493f54e74f8
Zachary-Kiz <zachary.kizell@gmail.com>
2026-05-27 12:16:11 -0400

    update user api files for client and server

--- api/userClient.ts
+++ api/userClient.ts
@@ -1,7 +1,4 @@
-"use server"
-
 import { LoginData, SignUpData } from "@/types/user";
-import { cookies } from "next/headers";
 
 const BACKEND_API = process.env.NEXT_PUBLIC_API;
 
@@ -59,14 +56,12 @@ export async function login(username: string, password : string) {
             credentials: 'include'
         });
 
-        console.log(res.headers.getSetCookie());
-
         if (!res.ok) {
             let message = `Request failed: ${res.status}`;
 
         try {
-            const errorBody = await res.text();
-            message = errorBody || message;
+            const errorBody = await res.json();
+            message = errorBody.error || message;
         } catch {}
 
         throw new Error(message);
@@ -75,66 +70,6 @@ export async function login(username: string, password : string) {
         return true;
     } catch (err) {
         console.error("registerAccount error:", err);
-
-        throw err;
-    }
-}
-
-export async function validateToken() {
-    try {
-
-        const cookieStore = await cookies()
-
-        const res = await fetch(`${BACKEND_API}/auth/user/refresh`, {
-            headers: {
-                'Cookie': cookieStore.toString(),
-            },
-        });
-
-        if (!res.ok) {
-            let message = `Request failed: ${res.status}`;
-
-            try {
-                const errorBody = await res.text();
-                message = errorBody || message;
-            } catch {}
-
-            throw new Error(message);
-        }
-
-        return await res.json();
-    } catch (err) {
-        console.error("registerAccount error:", err);
-
-        throw err;
-    }
-}
-
-export async function getUser() {
-    try {
-
-        const cookieStore = await cookies()
-
-        const res = await fetch(`${BACKEND_API}/auth/user`, {
-            headers: {
-                'Cookie': cookieStore.toString(),
-            },
-        });
-
-        if (!res.ok) {
-            let message = `Request failed: ${res.status}`;
-
-            try {
-                const errorBody = await res.text();
-                message = errorBody || message;
-            } catch {}
-
-            throw new Error(message);
-        }
-
-        return await res.json();
-    } catch (err) {
-        console.error("getUser error:", err);
 
         throw err;
     }
