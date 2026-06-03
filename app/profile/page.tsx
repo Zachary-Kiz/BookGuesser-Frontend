f69864e47cb410d07240b42b49dd70f39b773722
Zachary-Kiz <zachary.kizell@gmail.com>
2026-06-03 19:19:45 -0400

    display chart of user success on profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,12 @@
-import { getUser, validateToken } from "@/api/userServer"
+import { getUser } from "@/api/userServer"
 import { redirect } from "next/navigation";
+import Image from 'next/image';
+import styles from "./Profile.module.css"
+import { Bar } from "react-chartjs-2";
+import { BarElement, CategoryScale, Chart as Chartjs, Legend, LinearScale, Tooltip} from 'chart.js'
+import ChartWrapper from "@/components/ChartWrapper/ChartWrapper";
+
+
 
 
 export default async function Profile() {
@@ -7,10 +14,21 @@ export default async function Profile() {
     const user = await getUser();
     if (!user) redirect("/login")
     const username = user['user']
+
     
     return (
-        <div>
-            {user ? username : "Profiles coming soon!"}
+        <div className={styles.profileContainer}>
+            <div className="flex flex-row justify-center items-center gap-4 p-4">
+                <Image className={styles.profilePic} src="/profile.png" alt="profile picture" width={100} height={100}></Image>
+                <h2 className="text-3xl">{username}</h2>
+            </div>
+            <div className={styles.profileChart}>
+                <div className="text-center"><b>Total Guesses Distribution</b></div>
+                <div>This chart shows the distribution of successful guesses across all daily puzzles. Each bar represents the number of times a puzzle was solved in a specific number of guesses.</div>
+                 <ChartWrapper stats={user['stats']}></ChartWrapper>
+            </div>
+           
+            
         </div>
     )
 }
---
+++ components/ChartWrapper/ChartWrapper.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import {Chart as Chartjs, BarElement, CategoryScale, Legend, LinearScale, Tooltip } from "chart.js"
+import { Bar } from "react-chartjs-2"
+
+interface ChartWrapperType {
+    stats : Object
+}
+
+Chartjs.register(
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Tooltip,
+    Legend
+)
+
+export default function ChartWrapper({stats} : ChartWrapperType) {
+
+    const data = {
+        labels: ["1","2","3","4","5","6","failed"],
+        datasets : [
+            {
+                label: '# of Guesses',
+                data : Object.values(stats),
+                backgroundColor : '#4B2E2B',
+            }
+        ]
+    }
+
+    const options = {
+
+    }
+
+    return (
+        <Bar data={data} options={options}>
+        </Bar>
+    )
+}
