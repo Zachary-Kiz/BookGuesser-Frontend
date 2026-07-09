88815c3f3a5d55ed61627f2e0c2581522ce68edc
Zachary-Kiz <zachary.kizell@gmail.com>
2026-07-09 12:52:29 -0400

    add pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,5 +6,33 @@ export default function sitemap(): MetadataRoute.Sitemap {
             url: "https://bookguesser.app",
             lastModified: new Date(),
         },
+        {
+            url: "https://bookguesser.app/today",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://bookguesser.app/archive",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://bookguesser.app/login",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://bookguesser.app/sign_up",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://bookguesser.app/profile",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://bookguesser.app/puzzle",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://bookguesser.app/logout",
+            lastModified: new Date(),
+        },
     ];
 }
